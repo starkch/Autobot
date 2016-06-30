8d8f7958afda312525328c4718547b3ea5c199b7
Chip Stark <starkc@ads.autodesk.com>
2016-06-30 14:39:45 -0400

    add travis support, use public parameters

--- config.js
+++ config.js
@@ -1,10 +1,16 @@
 
- var config = {
+var config = {
+  // nick
   //username : 't-sippn',
   //repo : 'orion-docs',
-  
-  username : 'starkc',
-  repo : 'livereview',
+
+  // Autodesk
+  //username : 'starkc',
+  //repo : 'livereview',
+
+  // public
+  username : 'starkch',
+  repo : 'autobot',
   
   hookUrl : 'http://localhost:4568/webhook',
   localHostUrl : 'http://1bbd94a2.ngrok.io/webhook',
@@ -15,9 +21,9 @@
  config.baseUrl = 'https://github.com/' + config.username + '/' + config.repo;
 
 // public github
-// config.apiUrl = 'https://api.github.com/repos/' + config.username + '/' + config.repo;
+ config.apiUrl = 'https://api.github.com/repos/' + config.username + '/' + config.repo;
 
 // enterprise, autodesk, github
- config.apiUrl = 'https://git.autodesk.com/api/v3/repos/' + config.username + '/' + config.repo;
+// config.apiUrl = 'https://git.autodesk.com/api/v3/repos/' + config.username + '/' + config.repo;
 
 module.exports =  config;
